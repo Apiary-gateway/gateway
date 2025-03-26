6794fa0e691d0e5ce0ecbc8433531432034035eb
kjpoccia <kjpsubs@gmail.com>
2025-03-26 10:14:55 -0400

    Upgrade to CDK v2, clean up dependencies

--- lib/ai-gateway-stack.ts
+++ lib/ai-gateway-stack.ts
@@ -1,14 +1,15 @@
 import { Stack, StackProps, Duration, RemovalPolicy, aws_logs, SecretValue } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as lambdaNode from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as dotenv from 'dotenv';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import { 
+  aws_lambda as lambda,
+  aws_lambda_nodejs as lambdaNode,
+  aws_apigateway as apigateway,
+  aws_secretsmanager as secretsmanager,
+  aws_dynamodb as dynamodb,
+  aws_iam as iam,
+  aws_logs as logs,
+} from 'aws-cdk-lib';
 
-dotenv.config();
 
 export class AiGatewayStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
