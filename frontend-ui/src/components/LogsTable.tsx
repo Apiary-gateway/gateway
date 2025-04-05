ed44c1dc2d25e514926e87a0e7fa4d27c664ec29
Shwetank Tewari <tank@Shwetanks-Mac-mini.local>
2025-04-05 18:19:25 -0400

    correctly formatted status

--- frontend-ui/src/components/LogsTable.tsx
+++ frontend-ui/src/components/LogsTable.tsx
@@ -45,7 +45,11 @@ function LogsTable({
             <tr key={log.id}>
               <td>{formatDateTime(log.timestamp)}</td>
               <td>{log.thread_id || 'NA'}</td>
-              <td className={!log.is_successful ? 'status-error' : ''}>
+              <td
+                className={
+                  !log.is_successful ? 'status-error' : 'status-success'
+                }
+              >
                 {log.success_reason || log.error_reason || 'NA'}
               </td>
               <td>{log.model || 'NA'}</td>
