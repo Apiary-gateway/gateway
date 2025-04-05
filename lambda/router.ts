048340f8754687d299a3e638bc0b619dcbf73b59
Shwetank Tewari <tank@mac.mynetworksettings.com>
2025-04-04 20:28:07 -0400

    deployed stack

--- lambda/router.ts
+++ lambda/router.ts
@@ -1,77 +1,69 @@
-import { validateRequest } from "./util/validateRequest";
-import { extractRequestData, extractRequestMetadata } from "./util/extractRequestData";
-import { getMessageHistory, saveMessages } from "./util/getAndSaveMessages";
-import { routeRequest } from "./util/routeRequest";
-import { isValidModelForProvider } from "./util/modelValidation";
-// import {
-//     logSuccessfulRequest,
-//     logFailedRequest,
-//     CommonLogData,
-// } from './util/logger';
-import { parse } from 'path';
-import { calculateCost } from "./util/calculateCost";
+import { validateRequest } from './util/validateRequest';
+import {
+  extractRequestData,
+  extractRequestMetadata,
+} from './util/extractRequestData';
+import { getMessageHistory, saveMessages } from './util/getAndSaveMessages';
+import { routeRequest } from './util/routeRequest';
+import { isValidModelForProvider } from './util/modelValidation';
+// import { parse } from 'path';
+// import { calculateCost } from "./util/calculateCost";
 
 export const handler = async (event: unknown) => {
-    // const logData: CommonLogData = {
-    //     requestStartTime: Date.now(),
-    //     provider: null,
-    //     model: null,
-    //     tokens_used: 0,
-    //     cost: 0,
-    //     RawRequest: JSON.stringify(event),
-    // };
+  try {
+    const payload = validateRequest(event);
+    const { threadID, prompt, provider, model, userId } =
+      extractRequestData(payload);
+    const metadata = extractRequestMetadata(event, payload);
 
-    try {
-      const payload = validateRequest(event);
-      const { threadID, prompt, provider, model, userId } = extractRequestData(payload);
-      const metadata = extractRequestMetadata(event, payload);
+    const history = await getMessageHistory(threadID);
+    const response = await routeRequest({
+      history,
+      prompt,
+      provider,
+      model,
+      metadata,
+    });
 
-      const history = await getMessageHistory(threadID);
-      const response = await routeRequest({ history, prompt, provider, model, metadata });
-      
-      await saveMessages(prompt, response.text, threadID); 
+    await saveMessages(prompt, response.text, threadID);
 
-      // await logSuccessfulRequest({
-      //     ...logData,
-      //     RawResponse: JSON.stringify(response),
-      // });
-      console.log(
-          userId,
-          threadID,
-          response.text, 
-          response.provider, 
-          response.model, 
-          response.log, 
-          response.usage?.completion_tokens, 
-          response.usage?.prompt_tokens,
-          // response.simpleCacheHit,
-          // response.semanticCacheHit
-      )
+    console.log(
+      userId,
+      threadID,
+      response.text,
+      response.provider,
+      response.model,
+      response.log,
+      response.usage?.completion_tokens,
+      response.usage?.prompt_tokens
+      // response.simpleCacheHit,
+      // response.semanticCacheHit
+    );
 
-      return {
-          statusCode: 200,
-          headers: {
-            "simple-cache": `${response.simpleCacheHit ? 'HIT' : 'MISS'}`,
-            "semantic-cache": `${response.semanticCacheHit ? 'HIT' : 'MISS'}`,
-          },
-          body: JSON.stringify({
-              threadID,
-              response
-          }),
-      };
-    } catch (error) {
-        console.error(error);
+    return {
+      statusCode: 200,
+      headers: {
+        'simple-cache': `${response.simpleCacheHit ? 'HIT' : 'MISS'}`,
+        'semantic-cache': `${response.semanticCacheHit ? 'HIT' : 'MISS'}`,
+      },
+      body: JSON.stringify({
+        threadID,
+        response,
+      }),
+    };
+  } catch (error) {
+    console.error(error);
 
-        const errorMessage =
-          error instanceof Error ? error.message : 'Unknown Error';
-    
-        // await logFailedRequest({ ...logData, errorMessage });
-    
-        return {
-          statusCode: 500,
-          body: JSON.stringify({
-            error: errorMessage,
-          }),
-        }
-    }
-}
+    const errorMessage =
+      error instanceof Error ? error.message : 'Unknown Error';
+
+    // await logFailedRequest({ ...logData, errorMessage });
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: errorMessage,
+      }),
+    };
+  }
+};
